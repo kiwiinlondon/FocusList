38b114106e9b3249fda0011ac6bffa4478d0f7a0
Geoff Poore <g.poore@odey.com>
2014-01-29 11:28:53 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/OdeyFocusList@4449 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.FocusList.Contracts/IFocusList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odey.FocusList.Contracts
+{
+    [ServiceContract(Namespace = "Odey.ClientServices.Contracts")]
+    public interface IFocusList
+    {
+        [OperationContract]
+        void ImportExisting();
+    }
+}
---
+++ Odey.FocusList.FocusListService/FocusListService.svc.cs
@@ -0,0 +1,19 @@
+﻿using Odey.FocusList.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Odey.FocusList.FocusListService
+{
+    public class FocusListService : IFocusList
+    {       
+        public void ImportExisting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
